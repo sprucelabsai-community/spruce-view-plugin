c53e9764e2bb3bbcfc3440106581756bb5a948eb
Taylor Romero <me@taylorrome.ro>
2021-11-11 19:40:33 -0700

    major: removed unneeded mock skill view controller (already in test-fixtures)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { default as plugin } from './plugins/view.plugin'
 export { default as AbstractViewControllerTest } from './tests/AbstractViewControllerTest'
-export { default as MockSkillViewController } from './tests/MockSkillViewController'
